8331a188fae868a8b40b199e15b717f789460ce8
Jos Martin <jlmartin@uk-jlmartin-deb7-64>
2014-12-01 12:01:20 +0000

    commit makeMpiArgsScript for UMICH

--- makeMpiArgsScript.m
+++ makeMpiArgsScript.m
@@ -1,17 +1,21 @@
+mpiexecCommand = '/usr/cac/rhel6/mpiexec/bin/mpiexec';
+mpiexecArgs = sprintf('-n %d', props.NumberOfTasks);
 
-import com.mathworks.toolbox.distcomp.local.SmpdDaemonManager
-% Get all the args from the SmpdGateway
-mpiargs = parallel.internal.apishared.SmpdGateway.getMpiexecArgs(variables(:,1));
-if ~SmpdDaemonManager.getManager.isDaemonInUse
-    SmpdDaemonManager.getManager.getPortAndIncrementUsage;
-end
-% Get the current port number for the daemon
-spmdPort = SmpdDaemonManager.getManager.getPortAndIncrementUsage;
-SmpdDaemonManager.getManager.releaseUsage;
-
-mpiexecArgs = sprintf('%s %s %s', ...
-    sprintf('%s ', mpiargs{2:end}), ...
-    sprintf('-port %d', spmdPort), ...
-    sprintf('-hosts 1 127.0.0.1 %d', props.NumberOfTasks) );
-
-mpiexecCommand = mpiargs{1};
+% BELOW is the code used to debug mpiexec issues on a single computer
+% running the local cluster smpd daemon
+% import com.mathworks.toolbox.distcomp.local.SmpdDaemonManager
+% % Get all the args from the SmpdGateway
+% mpiargs = parallel.internal.apishared.SmpdGateway.getMpiexecArgs(variables(:,1));
+% if ~SmpdDaemonManager.getManager.isDaemonInUse
+%     SmpdDaemonManager.getManager.getPortAndIncrementUsage;
+% end
+% % Get the current port number for the daemon
+% spmdPort = SmpdDaemonManager.getManager.getPortAndIncrementUsage;
+% SmpdDaemonManager.getManager.releaseUsage;
+% 
+% mpiexecArgs = sprintf('%s %s %s', ...
+%     sprintf('%s ', mpiargs{2:end}), ...
+%     sprintf('-port %d', spmdPort), ...
+%     sprintf('-hosts 1 127.0.0.1 %d', props.NumberOfTasks) );
+% 
+% mpiexecCommand = mpiargs{1};
